91fd3a6c7a5269703519fd1e99bdb58dfb9ba332
trinhngoc1612 <58604220+trinhngoc1612@users.noreply.github.com>
2020-01-17 17:38:34 +0700

    Add files via upload

--- views/components/Bottombar.js
+++ views/components/Bottombar.js
@@ -1,45 +1,45 @@
 let Bottombar = {
 	render: async () => {
 		let view =  /*html*/`
-		<footer class="footer">
-			<div class="container">
-				<div class="row">
-					<div class="logos">
-						<img src="assets/img/footer1.png">
-						<img src="assets/img/footer2.png">
-						<img src="assets/img/footer3.png">
-						<img src="assets/img/footer4.png">
-					</div>
-					<div class="wrapper-media">
-						<ul>
-							<li><i class="fab fa-facebook-square"></i></li>
-							<li><i class="fab fa-twitter-square"></i></li>
-							<li><i class="fab fa-instagram"></i></li>
-						</ul>
-					</div>
-				</div>
-
-				<div class="row">
-					<div class="img-left">
-						<img src="assets/img/footer5.jpg">
-					</div>
-					<div class="more-info">
-						<ul>
-							<li><a href="#">Privacy policy</a></li>
-							<li><a href="#">Terms of service</a></li>
-							<li><a href="#">Cookie policy</a></li>
-							<li><a href="#">Support</a></li>
-							<li><a href="#">Approved Partners</a></li>
-							<li><a href="#">Careers</a></li>
-						</ul>
-
-						<p>© Copyright Games Workshop Limited 2020. Warhammer, the Warhammer logo, GW, Games Workshop, The Game of Fantasy Battles, the twin-tailed comet logo, and all associated logos, illustrations, images, names, creatures, races, vehicles, locations, weapons, characters, and the distinctive likeness thereof, are either ® or TM, and/or © Games Workshop Limited, variably registered around the world, and used under licence. Developed by Creative Assembly and published by SEGA. Creative Assembly, the Creative Assembly logo, Total War and the Total War logo are either registered trademarks or trademarks of The Creative Assembly Limited. SEGA and the SEGA logo are either registered trademarks or trademarks of SEGA Holdings Co., Ltd. or its affiliates. All rights reserved. SEGA is registered in the U.S. Patent and Trademark Office. All other trademarks, logos and copyrights are property of their respective owners.</p>
-					</div>
-				</div>
-				
+<footer class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="logos">
+				<div><img src="assets/img/footer1.png"></div>
+				<div><img src="assets/img/footer2.png"></div>
+				<div><img src="assets/img/footer3.png"></div>
+				<div><img src="assets/img/footer4.png"></div>
 			</div>
-			<div></div>
-		</footer>
+			<div class="wrapper-media">
+				<ul>
+					<li><i class="fab fa-facebook-square"></i></li>
+					<li><i class="fab fa-twitter-square"></i></li>
+					<li><i class="fab fa-instagram"></i></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="img-left col-md-4 col-12">
+				<img src="assets/img/footer5.jpg">
+			</div>
+			<div class="more-info col-md-8 col-12">
+				<ul>
+					<li><a href="#">Privacy policy</a></li>
+					<li><a href="#">Terms of service</a></li>
+					<li><a href="#">Cookie policy</a></li>
+					<li><a href="#">Support</a></li>
+					<li><a href="#">Approved Partners</a></li>
+					<li><a href="#">Careers</a></li>
+				</ul>
+
+				<p>© Copyright Games Workshop Limited 2020. Warhammer, the Warhammer logo, GW, Games Workshop, The Game of Fantasy Battles, the twin-tailed comet logo, and all associated logos, illustrations, images, names, creatures, races, vehicles, locations, weapons, characters, and the distinctive likeness thereof, are either ® or TM, and/or © Games Workshop Limited, variably registered around the world, and used under licence. Developed by Creative Assembly and published by SEGA. Creative Assembly, the Creative Assembly logo, Total War and the Total War logo are either registered trademarks or trademarks of The Creative Assembly Limited. SEGA and the SEGA logo are either registered trademarks or trademarks of SEGA Holdings Co., Ltd. or its affiliates. All rights reserved. SEGA is registered in the U.S. Patent and Trademark Office. All other trademarks, logos and copyrights are property of their respective owners.</p>
+			</div>
+		</div>
+		
+	</div>
+	<div></div>
+</footer>
 		`
 		return view
 	},
--- views/components/Topbar.js
+++ views/components/Topbar.js
@@ -1,33 +1,41 @@
 let Topbar = {
 	render: async () => {
 		let view =  /*html*/`
-			<div class="topbar">
-				<div class="container">
-					<div class="wrapper-logo">
-						<a href="#/home"><h1>TOTAL WAR</h1></a>
-					</div>
-					<div class="wrapper-menu">
-						<ul class="nav">
-							<li class="nav-item menu-link"><a class="nav-link" href="#/access">access</a></li>
-							<li class="nav-item menu-link"><a class="nav-link" href="#/academy">academy</a></li>
-							<li class="nav-item menu-link"><a class="nav-link" href="#/games">games</a></li>
-							<li class="nav-item menu-link"><a class="nav-link" href="#/support">support</a></li>
-							<li class="nav-item menu-link"><a class="nav-link" href="#/blog">blog</a></li>
-							<li class="nav-item menu-link"><a class="nav-link" href="#/forums">forums</a></li>
-							<li class="nav-item menu-link"><a class="nav-link last-child" href="#/faq">faq</a></li>
-							<li><button class="btn-purchase btn-more">purchase</button></li>
-							<li><i class="fab fa-facebook-square"></i></li>
-							<li><i class="fab fa-twitter-square"></i></li>
-							<li><i class="fab fa-instagram"></i></li>
-						</ul>
-					</div>
-				</div>
+<div class="topbar">
+	<div class="container">
+		<div class="wrapper-logo">
+			<a href="/totalwar"><h1>TOTAL WAR</h1></a>
+		</div>
+		
+		<div class="wrapper-menu">
+			<div class="wrapper-button-menu">
+				<button class="btn btn-primary"><i class="fas fa-bars"></i></button>
 			</div>
+
+			<ul class="nav menu-topbar">
+				<li class="nav-item menu-link"><a class="nav-link" href="#/access">access</a></li>
+				<li class="nav-item menu-link"><a class="nav-link" href="#/academy">academy</a></li>
+				<li class="nav-item menu-link"><a class="nav-link" href="#/games">games</a></li>
+				<li class="nav-item menu-link"><a class="nav-link" href="#/support">support</a></li>
+				<li class="nav-item menu-link"><a class="nav-link" href="#/blog">blog</a></li>
+				<li class="nav-item menu-link"><a class="nav-link" href="#/forums">forums</a></li>
+				<li class="nav-item menu-link"><a class="nav-link last-child" href="#/faq">faq</a></li>
+				<li><button class="btn-purchase btn-more">purchase</button></li>
+				<li><i class="fab fa-facebook-square"></i></li>
+				<li><i class="fab fa-twitter-square"></i></li>
+				<li><i class="fab fa-instagram"></i></li>
+			</ul>
+		</div>
+	</div>
+</div>
 		  `
 		return view
 	},
-	after_render: async () => { }
-
+	after_render: async () => { 
+		$(".wrapper-button-menu button").click(function(){
+			$(".menu-topbar").toggle();
+		});
+	}
 }
 
 export default Topbar;
